21653d4ecb4e9a39e505962121a097ab2c678aa8
Nya Haseley-Ayende <nhaseleyayende@salesforce.com>
2023-06-08 12:00:48 -0700

    finished step 4

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // Global Constants
-const apiKey = "MY_API_KEY"
+const apiKey = "RXqwRIzTKNCuE32A6fSJFG4kiYoob3hv"
+const limit = 9
+const rating = "g"
+const searchFormQuery = document.getElementById(".search-form")
+const searchInputQuery = document.getElementById(".search-input")
+const searchButtonQuery = document.getElementById(".search-button")
+
+const GIPHY_API_BASE_URL = `http://api.giphy.com/v1/gifs/search`
+
+const state = {
+  apiPage: 0,
+  searchTerm: "",
+}
+
+
+/** Render div element for a single GIF. */
+function generateGifHTML(url) {
+  return `
+    <div class="gif">
+        <img src="${url}" />
+    </div>
+`
+}
+
 
 /**
  * Update the DOM to display results from the Giphy API query.
@@ -10,6 +33,12 @@ const apiKey = "MY_API_KEY"
  */
 function displayResults(results) {
   // YOUR CODE HERE
+  let gifsHTMLString = ""
+  for (let gif of results) {
+    gifsHTMLString += generateGifHTML(gif?.images?.original?.url ?? "")
+  }
+
+  resultsEl.innerHTML += gifsHTMLString
 }
 
 /**
@@ -21,6 +50,11 @@ function displayResults(results) {
  */
 async function getGiphyApiResults(searchTerm) {
   // YOUR CODE HERE
+  const offset = state.apiPage * limit
+  const response = await fetch(createGiphyEndpointUrl(searchTerm, limit, offset)) // await bc async function
+  const jsonResponse = await response.json() // await bc async function
+  console.log(jsonResponse.data)
+  return jsonResponse.data // async function
 }
 
 /**
@@ -31,6 +65,21 @@ async function getGiphyApiResults(searchTerm) {
  */
 async function handleFormSubmit(event) {
   // YOUR CODE HERE
+  event.preventDefault()
+  // disables the default handling of the form submission event, which will cause the page to reload
+  
+  // reset results display section
+  resultsEl.innerHTML = ""
+  // handle state changes
+  state.apiPage = 0
+  state.searchTerm = searchInput.value
+  const results = await getGiphyApiResults(state.searchTerm) // await bc async function
+  displayResults(results)
+  searchInput.value = ""
+  state.apiPage += 1
+  showMoreBtn?.classList?.remove?.("hidden")
+
+
 }
 
 // searchForm.addEventListener("submit", handleFormSubmit)
@@ -44,9 +93,17 @@ async function handleFormSubmit(event) {
  */
 async function handleShowMore(event) {
   // YOUR CODE HERE
+  const results = await getGiphyApiResults(state.searchTerm) // await bc async function
+  displayResults(results)
+  // currentApiPage++
+  state.apiPage += 1
 }
 
 window.onload = function () {
   // YOUR CODE HERE
+  // fetch("http://api.giphy.com/v1/gifs/search?api_key=${apiKey}" + "&q=puppy" + "&rating=${g}" + "&limit=${limit}").then((response) => response.json()).then((data) => {
+  //   // dont think variables are being picked up
+	//   console.log(data)
+  // });
   // Add any event handlers here
 }
